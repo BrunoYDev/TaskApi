71f497a2a1d1985ca3f46337aa57dd74d7af7f50
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2023-12-15 16:15:30 -0300

    Update: UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -14,6 +14,11 @@ namespace TaskApi.Controllers
     {
         private readonly IUserRepository _userRepository;
 
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<UserModel>>> ReturnAllUsers()
         {
@@ -34,6 +39,23 @@ namespace TaskApi.Controllers
             UserModel user = await _userRepository.AddUser(userModel);
 
             return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UserModel>> Update([FromBody] UserModel userModel,int id)
+        {
+            userModel.Id = id;
+
+            UserModel user = await _userRepository.UpdateUser(userModel,id);
+
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UserModel>> Delete(int id)
+        {
+            bool apagado = await _userRepository.Delete(id);
+            return Ok(apagado);
         }
     }
 }
